a6f5b09e50be5dee23710daeb46887ef9f1e27db
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-11-24 08:48:19 +0700

    Add graph drawing logic

--- scripts/linear-regression.js
+++ scripts/linear-regression.js
@@ -30,22 +30,72 @@ LinearRegressionModel.prototype.bindFitIteration = function(onFitIteration) {
 
 
 /* The View */
-function LinearRegressionView(element, clearButton, trainButton) {
+function LinearRegressionView(element, clearButton, trainButton, configuration) {
     //Set the element to draw to
     this.root = element;
+
     //Set the two buttons to clear datapoint and start training
     this.clearButton = clearButton;
     this.trainButton = trainButton;
-}
 
-LinearRegressionView.prototype.init = function() {
-    //Initialize a graph
+    //Configuration object: { width: ..., height: ..., margin: ... }
+    this.configuration = configuration;
 }
 
 LinearRegressionView.prototype.render = function(datapoints, m, b) {
+    //Render the canvas and axes
+    this.renderCanvas();
+    this.renderAxes();
+
     //Render the graph, all the points and the line mx + b
     this.renderPoints(datapoints);
     this.renderLine(m, b);
+}
+
+LinearRegressionView.prototype.renderCanvas = function() {
+    //Render the initial canvas layout
+
+    //Define the height and wdith of canvas
+    var margin = this.configuration.margin;
+    var width = this.configuration.width;
+    var height = this.configuration.height;
+
+    //Create SVG element
+    this.graph = d3.select(".graph__canvas")
+        .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .style("background-color", "#FFF")
+        .append("g")
+            .attr("transform", `translate(${margin}, ${margin})`);
+}
+
+LinearRegressionView.prototype.renderAxes = function() {
+    //Render the x and y axes
+
+    //Define the margin
+    var margin = this.configuration.margin;
+    var width = this.configuration.width - margin * 2;
+    var height = this.configuration.height - margin * 2;
+
+    //Create x and y scale
+    var scaleX = d3.scaleLinear()
+        .domain([0, 10])
+        .range([0, width]);
+
+    var scaleY = d3.scaleLinear()
+        .domain([0, 10])
+        .range([height, 0])
+
+    //Draw the axes
+    this.graph
+        .append("g")
+            .attr("transform", `translate(0, ${width})`)
+            .call(d3.axisBottom(scaleX));
+
+    this.graph
+        .append("g")
+            .call(d3.axisLeft(scaleY));
 }
 
 LinearRegressionView.prototype.renderPoints = function(datapoints) {
@@ -73,10 +123,17 @@ LinearRegressionView.prototype.bindStartFit = function(onStartFit) {
 
 
 /* The Controller */
-function LinearRegressionController(model, controller) {
+function LinearRegressionController(model, view) {
     //Set model and controller
     this.model = model;
-    this.controller = controller;
+    this.view = view;
+
+    //Bind the model callbacks
+
+    //Bind the view callbacks
+
+    //Render the view
+    this.view.render();
 }
 
 LinearRegressionController.prototype.handleClearPoints = function() {
@@ -94,3 +151,22 @@ LinearRegressionController.prototype.handleStartFit = function() {
 LinearRegressionController.prototype.handleFit = function() {
     //Handle fit iteration
 }
+
+//Initialize application
+const configuration = {
+    width: 600,
+    height: 600,
+    margin: 50
+}
+Object.freeze(configuration);
+
+const app = (function() {
+    //Create view, model and controller
+    var view = new LinearRegressionView(null, null, null, configuration);
+    var model =  new LinearRegressionModel();
+    var controller = new LinearRegressionController(model, view);
+
+    return {
+        //Methods for application
+    }
+})();
